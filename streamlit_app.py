a5ad468a30c098bbe6e4dddf4e136b8f20c29752
AltofN <altofnaufal1@gmail.com>
2025-06-23 16:47:59 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning Test App')
 
 st.info('Test of machine learning model')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
