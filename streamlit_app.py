28c882dab4abc79d3ad5c70aff0be70cf0d240b0
AltofN <altofnaufal1@gmail.com>
2025-06-25 21:18:52 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,17 +77,18 @@ with st.expander('Data preparation'):
 # Model training and inference
 ## Train the ML model
 clf = RandomForestClassifier()
-clf.fit(X,y)
+clf.fit(X, y)
 
-## Apply modelto make prediction
+## Apply model to make predictions
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
-df_prediction_proba.columns = ['Adelie', 'Chainstrap', 'Gentoo']
+df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba.rename(columns={0: 'Adelie',
-                                    1: 'Chinstrap',
-                                    2: 'Gentoo'})
+                                 1: 'Chinstrap',
+                                 2: 'Gentoo'})
+
 # df_prediction_proba
 
 # Display predicted species
