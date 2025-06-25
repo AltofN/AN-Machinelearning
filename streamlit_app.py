1ad1e79b4504f12375d445378902efd9ccc8fcc0
AltofN <altofnaufal1@gmail.com>
2025-06-25 21:20:08 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('Machine Learning Test App')
+st.title('🤖 Machine Learning App')
 
-st.info('Test of machine learning model')
+st.info('This is app builds a machine learning model!')
 
-with st.expander ('Data'):
+with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
@@ -16,28 +16,28 @@ with st.expander ('Data'):
   X_raw = df.drop('species', axis=1)
   X_raw
 
-  st.write('**Y**')
-  y_raw= df.species
+  st.write('**y**')
+  y_raw = df.species
   y_raw
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
-# Input feature
+# Input features
 with st.sidebar:
-  st.header('input features')
-  island = st.selectbox('island', ('Biscoe', 'Dream', 'Torgersen'))
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_lenght_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('Male', 'Female'))
-
-# Create a Dataframe for the input features
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
+  # Create a DataFrame for the input features
   data = {'island': island,
           'bill_length_mm': bill_length_mm,
           'bill_depth_mm': bill_depth_mm,
-          'flipper_lenght_mm': flipper_lenght_mm,
+          'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
@@ -63,7 +63,7 @@ target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 def target_encode(val):
-    return target_mapper[val]
+  return target_mapper[val]
 
 y = y_raw.apply(target_encode)
 
